0a934e7403ed2797ad19f31fb90c6a3656a9af76
dikshant bansal <dikshantbansal95@gmail.com>
2022-06-19 12:10:01 +0530

    Corousel added on home page

--- src/Features/Home/Home.js
+++ src/Features/Home/Home.js
@@ -1,11 +1,30 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/alt-text */
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import './Home.scss';
 
 const Home = () => {
   return (
     <div id="Home">
-      Home
+      <div className="corouselWrapper">
+        <Carousel autoPlay interval={3000}>
+          <div>
+            <img src="https://wallpaperaccess.com/full/151827.jpg" />
+          </div>
+          <div>
+            <img src="https://wallpaperaccess.com/full/735833.jpg" />
+          </div>
+          <div>
+            <img src="https://kcd.org.uk/wordpress/wp-content/uploads/2020/04/WEBSITE-Risen-Ascended-Glorified-1200x400.jpg" />
+          </div>
+          <div>
+            <img src="https://wallpaperaccess.com/full/1119361.jpg" />
+          </div>
+        </Carousel>
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
